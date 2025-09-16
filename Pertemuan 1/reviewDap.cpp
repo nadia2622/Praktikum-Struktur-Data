0f4bba928f0b3beaba3c02695d5c22578a001c76
nadia2622 <nadiatmbunan@gmail.com>
2025-09-16 17:52:21 +0700

    Tugas Review DAP

---
+++ Pertemuan 1/reviewDap.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <array>
+using namespace std;
+
+// int main() {
+//     int a;
+//     cout << "Masukkan angka: ";
+//     cin >> a;
+//     if(a == 5) {
+//         cout << "Ini adalah angka lima" << endl;
+//     } else {
+//         cout << "Ini bukan angka lima" << endl;
+//     }
+//     return 0;
+// }
+
+// int main() {
+//     int x;
+//     cout << "Masukkan x: ";
+//     cin >> x;
+//     if(x % 3 == 0) {
+//         cout << "x adalah bilangan kelipatan tiga" << endl;
+//     } else {
+//         cout << "x bukan bilangan kelipatan tiga" << endl;
+//     }
+//     return 0;
+// }
+
+// int main() {
+//     int x;
+//     int total = 0;
+//     int banyakAngka = 0;
+//     cout << "Masukkan nilai (akhiri dengan -999): " << endl;
+//     while (true) {
+//         cin >> x;
+
+//         if (x == -999) {
+//             break;
+//         }
+
+//         total += x;
+//         banyakAngka++;
+//     }
+//     if (banyakAngka == 0) {
+//         cout << 0 << endl;
+//     } else {
+//         double rata = (double) total / banyakAngka;
+//         cout << rata << endl;
+//     }
+//     return 0;
+// }
+
+
